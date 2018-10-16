4485ff12e0a6ab230391e5fbd4c40e9cc921fbcf
geovanifg <geofergon@msn.com>
2018-10-16 20:15:43 -0300

    aa

---
+++ Tupy.Extensions/DateTimeExtension.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Tupy.Extensions
+{
+    public static class DateTimeExtension
+    {
+        public static long ToLong(this DateTime self)
+        {
+            var text = self.ToString("yyyyMMddhhmmssfff");
+            long.TryParse(text, out long result);
+            return result;
+
+        }
+
+        public static string ToStringFull(this DateTime self, string separator)
+        {
+            var s = separator ?? "";
+            var result = self.ToString($"yyyy{s}MM{s}dd{s}hh{s}mm{s}ss{s}fff");
+            return result;
+        }
+
+        public static string ToStringYYYYMMDD(this DateTime self)
+        {
+            var result = self.ToString("yyyyMMdd");
+            return result;
+        }
+    }
+}
